e447091045c0900146cf26addd764c7775f03889
r0b0t <antoniomainakn@gmail.com>
2020-03-19 10:26:28 +0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:covid19/resources/app_colors.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      title: "Covid-19",
+      home: Home()
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: ListView(
+            children: <Widget>[
+              Text("App Bar"),
+              Text("Body")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
---
+++ lib/resources/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color PRIMARY = Color.fromRGBO(161, 43, 43, 1);
+  static const Color GREY = Color.fromRGBO(244, 244, 244, 1);
+}
