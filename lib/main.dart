3bc002b223cb115807ebe632cba2c507152acf25
Nancy Kinyanjui <nicy@Nancys-MacBook-Air.local>
2020-03-19 12:19:49 +0300

    feat/created the custom app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:covid19/resources/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,7 +23,43 @@ class Home extends StatelessWidget {
         child: Container(
           child: ListView(
             children: <Widget>[
-              Text("App Bar"),
+              Container(
+                margin: EdgeInsets.only(bottom: 12),
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                height: 48,
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      spreadRadius: 0,
+                      blurRadius: 6,
+                      offset: Offset(0, 7),
+                    )
+                  ]
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      child: Row(
+                        children: <Widget>[
+                          Image(
+                              image: AssetImage('assets/images/icon.png'),
+                              width: 36,
+                              height: 36,
+                              fit: BoxFit.contain,
+                          ),
+                          SizedBox(width: 12,),
+                          Text("Covid-19")
+                        ],
+                      ),
+                    ),
+                    Icon(Ionicons.ios_moon)
+                  ],
+                ),
+              ),
               Text("Body")
             ],
           ),
