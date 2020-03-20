3f22d37d21cccdf4a9f62eee467d0885852dd3f4
r0b0t <antoniomainakn@gmail.com>
2020-03-20 21:42:40 +0300

    fix/removed sf charts

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,13 @@ class Home extends StatelessWidget {
                               fit: BoxFit.contain,
                           ),
                           SizedBox(width: 12,),
-                          Text("Covid-19")
+                          Text(
+                              "Covid-19",
+                              style: TextStyle(
+                                color: AppColors.PRIMARY,
+                                fontSize: 16,
+                            ),
+                          )
                         ],
                       ),
                     ),
@@ -60,7 +66,224 @@ class Home extends StatelessWidget {
                   ],
                 ),
               ),
-              Text("Body")
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child:  Card(
+                  elevation: 5,
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                            "Cases",
+                            style: TextStyle(
+                              color: AppColors.BLUE,
+                              fontSize: 14
+                            ),
+                        ),
+                        SizedBox(height: 12),
+                        Container(
+                          height: 144,
+                          color: Colors.black12,
+                        )
+                      ],
+                    )
+                  )
+                ),
+              ),
+              SizedBox(height: 12),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Expanded(
+                      child: Card(
+                        elevation: 5,
+                        child: Container(
+                          padding: EdgeInsets.all(12),
+                          child: Column(
+                            children: <Widget>[
+                              Text(
+                                "Infected",
+                              ),
+                              SizedBox(height: 12),
+                              Container(
+                                height: 96,
+                                color: Colors.black12
+                              ),
+                              SizedBox(height: 12),
+                              Text("100%")
+                            ],
+                          )
+                        ),
+                      )
+                    ),Expanded(
+                      child: Card(
+                        elevation: 5,
+                        child: Container(
+                          padding: EdgeInsets.all(12),
+                          child: Column(
+                            children: <Widget>[
+                              Text(
+                                "Recovered",
+                              ),
+                              SizedBox(height: 12),
+                              Container(
+                                height: 96,
+                                color: Colors.black12
+                              ),
+                              SizedBox(height: 12),
+                              Text("48%")
+                            ],
+                          )
+                        ),
+                      )
+                    ),
+                    Expanded(
+                      child: Card(
+                        elevation: 5,
+                        child: Container(
+                          padding: EdgeInsets.all(12),
+                          child: Column(
+                            children: <Widget>[
+                              Text(
+                                "Dead",
+                              ),
+                              SizedBox(height: 12),
+                              Container(
+                                height: 96,
+                                color: Colors.black12
+                              ),
+                              SizedBox(height: 12),
+                              Text("2.3%")
+                            ],
+                          )
+                        ),
+                      )
+                    ),
+                  ],
+                )
+              ),
+              SizedBox(height: 12),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child:  Card(
+                    elevation: 5,
+                    child: Container(
+                        padding: EdgeInsets.all(8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(
+                                  "Most Affected Countries",
+                                  style: TextStyle(
+                                      color: AppColors.BLUE,
+                                      fontSize: 14
+                                  ),
+                                ),
+                                Text(
+                                  "View All",
+                                  style: TextStyle(
+                                      color: AppColors.BLUE,
+                                      fontSize: 14
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 12),
+                            Container(
+                                padding: EdgeInsets.all(12),
+                                child: Table(
+                                  children: [
+                                    TableRow(
+                                      children: [
+                                        Text("Country"),
+                                        Text("Infected"),
+                                        Text("Recovered"),
+                                        Text("Dead"),
+                                      ]
+                                    ),
+                                    TableRow(
+                                      children: [
+                                        Text("China"),
+                                        Text("1.7M"),
+                                        Text("970K"),
+                                        Text("3860"),
+                                      ]
+                                    ),
+                                    TableRow(
+                                      children: [
+                                        Text("Country"),
+                                        Text("Infected"),
+                                        Text("Infected"),
+                                        Text("Dead"),
+                                      ]
+                                    ),
+                                    TableRow(
+                                      children: [
+                                        Text("Country"),
+                                        Text("Country"),
+                                        Text("Infected"),
+                                        Text("Dead"),
+                                      ]
+                                    ),
+                                    TableRow(
+                                      children: [
+                                        Text("Country"),
+                                        Text("Country"),
+                                        Text("Infected"),
+                                        Text("Dead"),
+                                      ]
+                                    ),
+                                    TableRow(
+                                      children: [
+                                        Text("Country"),
+                                        Text("Infected"),
+                                        Text("Infected"),
+                                        Text("Dead"),
+                                      ]
+                                    ),
+
+                                  ],
+                                )
+                            )
+                          ],
+                        )
+                    )
+                ),
+              ),
+              SizedBox(height: 12),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child:  Card(
+                    elevation: 5,
+                    child: Container(
+                        padding: EdgeInsets.all(8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "World Map",
+                              style: TextStyle(
+                                  color: AppColors.BLUE,
+                                  fontSize: 14
+                              ),
+                            ),
+                            SizedBox(height: 12),
+                            Container(
+                                height: 144,
+                                color: Colors.black12
+                            )
+                          ],
+                        )
+                    )
+                ),
+              ),
             ],
           ),
         ),
--- lib/resources/app_colors.dart
+++ lib/resources/app_colors.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color PRIMARY = Color.fromRGBO(161, 43, 43, 1);
   static const Color GREY = Color.fromRGBO(244, 244, 244, 1);
+  static const Color BLUE = Color.fromRGBO(16, 16, 125, 1);
+
 }
