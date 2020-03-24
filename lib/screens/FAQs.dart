451143cddeea8c8df67c8e57605508c24141aa44
r0b0t <antoniomainakn@gmail.com>
2020-03-24 12:36:24 +0300

    feat/implemented the design for the FAQs screen

--- lib/screens/FAQs.dart
+++ lib/screens/FAQs.dart
@@ -10,7 +10,269 @@ class Faqs extends StatelessWidget {
       body: SafeArea(
         child: Container(
           child: ListView(
-            children: <Widget>[TopNavBar(), Text("Faqs")],
+            children: <Widget>[
+              TopNavBar(),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Card(
+                      elevation: 2,
+                      child: Padding(
+                        padding: EdgeInsets.all((12)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                  "lorem ipsum dolor il summet lorem ipsum dolor ill summet"),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.only(left: 12),
+                                child: Icon(Icons.chevron_right))
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 2,
+                      child: Padding(
+                        padding: EdgeInsets.all((12)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                  "lorem ipsum dolor il summet lorem ipsum dolor ill summet"),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.only(left: 12),
+                                child: Icon(Icons.chevron_right))
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 2,
+                      child: Padding(
+                        padding: EdgeInsets.all((12)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                  "lorem ipsum dolor il summet lorem ipsum dolor ill summet"),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.only(left: 12),
+                                child: Icon(Icons.chevron_right))
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 2,
+                      child: Padding(
+                        padding: EdgeInsets.all((12)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                  "lorem ipsum dolor il summet lorem ipsum dolor ill summet"),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.only(left: 12),
+                                child: Icon(Icons.chevron_right))
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 2,
+                      child: Padding(
+                        padding: EdgeInsets.all((12)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                  "lorem ipsum dolor il summet lorem ipsum dolor ill summet"),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.only(left: 12),
+                                child: Icon(Icons.chevron_right))
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 2,
+                      child: Padding(
+                        padding: EdgeInsets.all((12)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                  "lorem ipsum dolor il summet lorem ipsum dolor ill summet"),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.only(left: 12),
+                                child: Icon(Icons.chevron_right))
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 2,
+                      child: Padding(
+                        padding: EdgeInsets.all((12)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                  "lorem ipsum dolor il summet lorem ipsum dolor ill summet"),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.only(left: 12),
+                                child: Icon(Icons.chevron_right))
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 2,
+                      child: Padding(
+                        padding: EdgeInsets.all((12)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                  "lorem ipsum dolor il summet lorem ipsum dolor ill summet"),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.only(left: 12),
+                                child: Icon(Icons.chevron_right))
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 2,
+                      child: Padding(
+                        padding: EdgeInsets.all((12)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                  "lorem ipsum dolor il summet lorem ipsum dolor ill summet"),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.only(left: 12),
+                                child: Icon(Icons.chevron_right))
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 2,
+                      child: Padding(
+                        padding: EdgeInsets.all((12)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                  "lorem ipsum dolor il summet lorem ipsum dolor ill summet"),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.only(left: 12),
+                                child: Icon(Icons.chevron_right))
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 2,
+                      child: Padding(
+                        padding: EdgeInsets.all((12)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                  "lorem ipsum dolor il summet lorem ipsum dolor ill summet"),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.only(left: 12),
+                                child: Icon(Icons.chevron_right))
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 2,
+                      child: Padding(
+                        padding: EdgeInsets.all((12)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                  "lorem ipsum dolor il summet lorem ipsum dolor ill summet"),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.only(left: 12),
+                                child: Icon(Icons.chevron_right))
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 2,
+                      child: Padding(
+                        padding: EdgeInsets.all((12)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                  "lorem ipsum dolor il summet lorem ipsum dolor ill summet"),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.only(left: 12),
+                                child: Icon(Icons.chevron_right))
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 2,
+                      child: Padding(
+                        padding: EdgeInsets.all((12)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                  "lorem ipsum dolor il summet lorem ipsum dolor ill summet"),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.only(left: 12),
+                                child: Icon(Icons.chevron_right))
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
