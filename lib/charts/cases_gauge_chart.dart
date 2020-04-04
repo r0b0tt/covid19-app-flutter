8e74ccf37fcc7746923db4786e5dd80070144f17
r0b0t <antoniomainakn@gmail.com>
2020-04-04 21:21:48 +0300

    refactor/cleaned up code for the gauge charts

--- lib/charts/cases_gauge_chart.dart
+++ lib/charts/cases_gauge_chart.dart
@@ -10,32 +10,29 @@ class GaugeChart extends StatelessWidget {
 
   GaugeChart(this.seriesList, this.type, {this.animate});
 
-  /// Creates a [PieChart] with sample data and no transition.
   factory GaugeChart.withSampleData(String type) {
     return new GaugeChart(
       _createSampleData(type),
       type,
-      // Disable animations for image tests.
       animate: true,
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return new charts.PieChart(seriesList,
-        animate: animate,
-        // Configure the width of the pie slices to 30px. The remaining space in
-        // the chart will be left as a hole in the center. Adjust the start
-        // angle and the arc length of the pie so it resembles a gauge.
-        defaultRenderer: new charts.ArcRendererConfig(
-            arcWidth: 25, startAngle: 8 / 5 * pi, arcLength: 2 * pi));
+    return new charts.PieChart(
+      seriesList,
+      animate: animate,
+      defaultRenderer: new charts.ArcRendererConfig(
+          arcWidth: 14, startAngle: 8 / 5 * pi, arcLength: 2 * pi),
+    );
   }
 
   /// Create one series with sample hard coded data.
   static List<charts.Series<GaugeSegment, String>> _createSampleData(
       String type) {
     final data = [
-      new GaugeSegment('People', 75),
+      new GaugeSegment('People', 1),
     ];
 
     return [
@@ -53,7 +50,6 @@ class GaugeChart extends StatelessWidget {
   }
 }
 
-/// Sample data type.
 class GaugeSegment {
   final String segment;
   final int size;
