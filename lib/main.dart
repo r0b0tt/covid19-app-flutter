c4461f2bf157efeaada15b341872945149cf1221
r0b0t <antoniomainakn@gmail.com>
2020-05-01 11:43:27 +0300

    feat: integrated firebase analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:covid19/components/Navigation.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  Crashlytics.instance.enableInDevMode = true;
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
